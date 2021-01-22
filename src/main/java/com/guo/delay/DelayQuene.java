ba0fb5b214b278357d966133d92bd8d02c5eb595
guolejian <guolejian@126.com>
2021-01-22 17:42:56 +0800

    aaa

---
+++ src/main/java/com/guo/delay/DelayQuene.java
@@ -0,0 +1,17 @@
+package com.guo.delay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Date: 2021/1/22 17:29
+ * @Author 郭乐建
+ * @Since JDK 1.8
+ * @Description:
+ */
+@SpringBootApplication
+public class DelayQuene {
+    public static void main(String[] args) {
+        SpringApplication.run(DelayQuene.class, args);
+    }
+}
